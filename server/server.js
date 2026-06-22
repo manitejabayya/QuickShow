e5e86d468009f380ff9c2377f20aaf987b639e08
manitejabayyaa <maniteja.bayya369@gmail.com>
2026-06-22 11:21:41 +0530

    working on the backend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB4 from './config/db';
+import connectDB from './config/db';
+import {clerkMiddleware} from '@clerk/express';
 
 const app = express();
 const port = 3000;
 
+await connectDB();
+
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware);
 
 app.get('/',(req ,res)=>{
     res.send("server is Live")
